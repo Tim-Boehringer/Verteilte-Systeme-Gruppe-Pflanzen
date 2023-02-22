0d3b771e527943b83431e0be69085ee8851310c3
Isa217 <116289702+Isa217@users.noreply.github.com>
2023-02-22 10:38:33 +0100

    index

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,5 @@ const port = 4000;
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
 });
+mongoose.connect("mongodb://mongo:27017/test").then(() => {
+  console.log("Database connected");
