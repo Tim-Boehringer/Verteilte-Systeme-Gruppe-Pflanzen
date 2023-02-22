63c9a5839695469deb5d6690d59c9b8dff47335d
Tim-Boehringer <tim.boehringer02@gmail.com>
2023-02-22 10:59:46 +0100

    added the router to the index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const port = 4000;
 
+app.use(bodyParser.json());
+app.use("/plants", router);
+
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
 });
